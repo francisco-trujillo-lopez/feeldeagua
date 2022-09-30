c9352c7d478833ee09162e261beb89770a827536
francisco-trujillo-lopez <francisco.trujillo.dev@gmail.com>
2022-09-30 20:58:09 -0300

    remove comments

--- components/album/album/album.tsx
+++ components/album/album/album.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export const Album = ({ album }: any) => {
-	console.log("ALBUM: ", album);
-
 	return (
 		<div className="h-[400px] w-[400px] mb-10 mt-10">
 			<div className="h-[350px] w-[350px] m-4">
